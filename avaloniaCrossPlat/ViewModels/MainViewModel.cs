af35033fc40566218fe55767ad98365348530741
Simon <registerandothers@tutanota.com>
2025-12-21 23:34:11 +0100

    progressbar

--- avaloniaCrossPlat/ViewModels/MainViewModel.cs
+++ avaloniaCrossPlat/ViewModels/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -26,9 +28,14 @@ public partial class MainViewModel : ViewModelBase
     [ObservableProperty]
     private ObservableCollection<DataViewModel> _dataPourcent = new ObservableCollection<DataViewModel>();
 
+    [ObservableProperty]
+    private double _refreshProgress = 100; // 0 → 100
+    private readonly TimeSpan _refreshInterval = TimeSpan.FromSeconds(30);
+    private readonly TimeSpan _uiTick = TimeSpan.FromMilliseconds(100);
+    private DateTime _lastRefresh;
 
     public MainViewModel()
-    {        
+    {
         if (!String.IsNullOrEmpty(EndPoint))
         {
             LoadData();
@@ -71,34 +78,36 @@ public partial class MainViewModel : ViewModelBase
             var array = root.EnumerateArray();
             Console.WriteLine("5");
 
+            List<DataViewModel> tempData = new List<DataViewModel>(); ;
             foreach (var element in array)
             {
                 try
                 {
-                    
-                var data = new DataViewModel
-                {
-                    Id = element.GetProperty("id").GetString(),
-                    Name = element.GetProperty("name").GetString(),
-                    Value = element.GetProperty("value").GetString(),
-                    Unit = element.GetProperty("unit").GetString()
-                };
-                if (data != null)
+
+                    var data = new DataViewModel
+                    {
+                        Id = element.GetProperty("id").GetString(),
+                        Name = element.GetProperty("name").GetString(),
+                        Value = element.GetProperty("value").GetString(),
+                        Unit = element.GetProperty("unit").GetString()
+                    };
+                    if (data != null)
+                    {
+
+                        if (!String.IsNullOrEmpty(data.Name))
+                            data.Favorit = IsFavorite(data.Name);
+                        tempData.Add(data);
+                    }
+                }
+                catch (Exception)
                 {
 
-                    if (!String.IsNullOrEmpty(data.Name))
-                        data.Favorit = IsFavorite(data.Name);
-                    Data.Add(data);
-                }
-                }catch(Exception)
-                {
-                    
                 }
 
             }
             Console.WriteLine("3");
             string tempUnit = "";
-            foreach (var data in Data.OrderBy(x => x.Unit))
+            foreach (var data in tempData.OrderBy(x => x.Unit))
             {
                 if (tempUnit != data.Unit && data.Unit != null)
 
@@ -110,12 +119,18 @@ public partial class MainViewModel : ViewModelBase
                 Console.WriteLine($"{data.Name}");
             }
             Console.WriteLine("6");
-            DataTemp = new ObservableCollection<DataViewModel>(Data.Where(x => x.Unit == " °C").OrderBy(x => x.Favorit));
-            DataPourcent = new ObservableCollection<DataViewModel>(Data.Where(x => x.Unit == " %").OrderBy(x => x.Favorit));
             Console.WriteLine("7");
-            if (Data != null && Data.Count > 0)
+            if (tempData != null && tempData.Count > 0)
             {
-                IsDataValid = true;
+                DataTemp = new ObservableCollection<DataViewModel>(tempData.Where(x => x.Unit == " °C").OrderBy(x => x.Favorit));
+                DataPourcent = new ObservableCollection<DataViewModel>(tempData.Where(x => x.Unit == " %").OrderBy(x => x.Favorit));
+                Data = new ObservableCollection<DataViewModel>(tempData);
+                if (IsDataValid == null || !(bool)IsDataValid)
+                {
+                    IsDataValid = true;
+                    StartGlobalTimer();
+                    StartAnimationTimer();
+                }
             }
         }
         catch (Exception ex)
@@ -124,6 +139,45 @@ public partial class MainViewModel : ViewModelBase
 
             IsDataValid = false;
         }
+    }
+
+    private void StartGlobalTimer()
+    {
+        var timer = new DispatcherTimer()
+        {
+            Interval = _refreshInterval
+        };
+        timer.Tick += async (_, __) =>
+        {
+            _lastRefresh = DateTime.UtcNow;
+            RefreshProgress = 100;
+            await RefreshDataAsync();
+
+        };
+
+        timer.Start();
+        _lastRefresh = DateTime.UtcNow;
+    }
+    private void StartAnimationTimer()
+    {
+        var timer = new DispatcherTimer()
+        {
+            Interval = _uiTick
+        };
+        timer.Tick += async (_, __) =>
+        {
+            var elapsed = DateTime.UtcNow - _lastRefresh;
+            var ratio = elapsed.TotalMilliseconds / _refreshInterval.TotalMilliseconds;
+
+            RefreshProgress = Math.Max(0, 100 - ratio * 100);
+        };
+
+        timer.Start();
+    }
+
+    private async Task RefreshDataAsync()
+    {
+        LoadData();
     }
 
     private bool? IsFavorite(string name)
